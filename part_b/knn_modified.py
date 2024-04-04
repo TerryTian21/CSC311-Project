063c0607f08437cf1154fd3d729bbabbfb776186
Charlie Tao <charlietaoxl@gmail.com>
2024-04-04 17:33:58 -0400

    eval gender and premium

--- part_b/knn_modified.py
+++ part_b/knn_modified.py
@@ -24,7 +24,7 @@ def knn_impute_by_user(matrix, valid_data, k):
     # We use NaN-Euclidean distance measure.
     mat = nbrs.fit_transform(matrix)
     acc = sparse_matrix_evaluate(valid_data, mat)
-    # print("Validation Accuracy: {}".format(acc))
+    print("Validation Accuracy: {}".format(acc))
     return acc
 
 
@@ -35,6 +35,7 @@ def normalize_array(array):
 
 
 def scale_array(array, scale_factor):
+    print(array == array * 1.0)
     return array * scale_factor
 
 
@@ -95,15 +96,50 @@ def get_premium_array(student_meta_data):
     )
     return user_premium
 
+def get_gender_array(student_meta_data):
+    gender_array = np.array(
+        [
+            student_meta_data["gender"][i] if student_meta_data["gender"][i] is not None else np.nan
+            for i in range(len(student_meta_data["user_id"]))
+        ]
+    )
+    return gender_array
+
 def eval_adding_premium(sparse_matrix, student_meta_data, val_data):
-    sparse_matrix = append_data_as_new_question(sparse_matrix, get_premium_array(student_meta_data))
+    new_sparse_matrix = append_data_as_new_question(sparse_matrix, get_premium_array(student_meta_data))
 
     valid_acc = []
     for k in [11]:
-        print("k == ", k)
-        valid_acc.append(knn_impute_by_user(sparse_matrix, val_data, k))
+        print("\nk == ", k)
+        valid_acc.append(knn_impute_by_user(new_sparse_matrix, val_data, k))
+
+    scale_factors = [0.1, 0.9]
+    for scale in scale_factors:
+        print("\nPremium scale == ", scale)
+        new_sparse_matrix = append_data_as_new_question(sparse_matrix, get_premium_array(student_meta_data) * scale)
+        valid_acc.append(knn_impute_by_user(new_sparse_matrix, val_data, 11))
 
     print(valid_acc)
+    print("Max accuracy: ", max(valid_acc))
+    return append_data_as_new_question(sparse_matrix, get_premium_array(student_meta_data) * scale_factors[np.argmax(valid_acc) - 1])
+
+def eval_adding_gender(sparse_matrix, student_meta_data, val_data):
+    new_sparse_matrix = append_data_as_new_question(sparse_matrix, get_gender_array(student_meta_data))
+
+    valid_acc = []
+    for k in [11]:
+        print("\nk == ", k)
+        valid_acc.append(knn_impute_by_user(new_sparse_matrix, val_data, k))
+
+    scale_factors = [0.1, 0.5]
+    for i, scale in enumerate(scale_factors):
+        print("\nGender scale == ", scale)
+        new_sparse_matrix = append_data_as_new_question(sparse_matrix, get_gender_array(student_meta_data) * scale)
+        valid_acc.append(knn_impute_by_user(new_sparse_matrix, val_data, 11))
+
+    print(valid_acc)
+    print("Max accuracy: ", max(valid_acc), "at scale factor: ", scale_factors[np.argmax(valid_acc)-1])
+    return append_data_as_new_question(sparse_matrix, get_gender_array(student_meta_data) * scale_factors[np.argmax(valid_acc)-1])
 
 def main():
     # Get the root of the project
@@ -118,41 +154,44 @@ def main():
     normalized_year_array = normalize_array(year_array)
     # month_array = get_month_array(student_meta_data)
 
-    k_values = [6, 11, 16, 21, 26]
-    # Not normalized with year
-    sparse_matrix = append_data_as_new_question(sparse_matrix, year_array)
+    new_sparse = eval_adding_premium(sparse_matrix, student_meta_data, val_data)
+    new_sparse = eval_adding_gender(new_sparse, student_meta_data, val_data)
 
-    valid_acc = []
-    for k in k_values:
-        valid_acc.append(knn_impute_by_user(sparse_matrix, val_data, k))
+    # k_values = [6, 11, 16, 21, 26]
+    # # Not normalized with year
+    # sparse_matrix = append_data_as_new_question(sparse_matrix, year_array)
 
-    print("Not Normalized")
-    for i in range(len(valid_acc)):
-        print(f"Validation Accuracy for k = {k_values[i]} is {valid_acc[i]:.4f}")
+    # valid_acc = []
+    # for k in k_values:
+    #     valid_acc.append(knn_impute_by_user(sparse_matrix, val_data, k))
 
-    # Normalized with year
-    sparse_matrix = append_data_as_new_question(sparse_matrix, normalized_year_array)
-    valid_acc = []
-    for k in k_values:
-        valid_acc.append(knn_impute_by_user(sparse_matrix, val_data, k))
+    # print("Not Normalized")
+    # for i in range(len(valid_acc)):
+    #     print(f"Validation Accuracy for k = {k_values[i]} is {valid_acc[i]:.4f}")
 
-    print("Normalized")
-    for i in range(len(valid_acc)):
-        print(f"Validation Accuracy for k = {k_values[i]} is {valid_acc[i]:.4f}")
+    # # Normalized with year
+    # sparse_matrix = append_data_as_new_question(sparse_matrix, normalized_year_array)
+    # valid_acc = []
+    # for k in k_values:
+    #     valid_acc.append(knn_impute_by_user(sparse_matrix, val_data, k))
 
-    # Scaled with year
-    scale_factors = [0.1, 0.5, 1, 2, 10]
-    valid_acc = []
-    for scale_factor in scale_factors:
-        sparse_matrix = append_data_as_new_question(
-            sparse_matrix, scale_array(normalized_year_array, scale_factor)
-        )
-        valid_acc.append(knn_impute_by_user(sparse_matrix, val_data, 11))
+    # print("Normalized")
+    # for i in range(len(valid_acc)):
+    #     print(f"Validation Accuracy for k = {k_values[i]} is {valid_acc[i]:.4f}")
 
-    print("Scaled (with normalized year)")
-    for i, scale_factor in enumerate(scale_factors):
-        print(f"Scaled by {scale_factor}")
-        print(f"Validation Accuracy for k = {11} is {valid_acc[i]:.4f}")
+    # # Scaled with year
+    # scale_factors = [0.1, 0.5, 1, 2, 10]
+    # valid_acc = []
+    # for scale_factor in scale_factors:
+    #     sparse_matrix = append_data_as_new_question(
+    #         sparse_matrix, scale_array(normalized_year_array, scale_factor)
+    #     )
+    #     valid_acc.append(knn_impute_by_user(sparse_matrix, val_data, 11))
+
+    # print("Scaled (with normalized year)")
+    # for i, scale_factor in enumerate(scale_factors):
+    #     print(f"Scaled by {scale_factor}")
+    #     print(f"Validation Accuracy for k = {11} is {valid_acc[i]:.4f}")
 
     #####################################################################
     # TODO:                                                             #
