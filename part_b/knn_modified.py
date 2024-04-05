540212f61644f93d53b2fbcca7ca6313c5b611ee
Charlie Tao <charlietaoxl@gmail.com>
2024-04-05 16:56:58 -0400

    plotting analysis with partitioned test data

--- part_b/knn_modified.py
+++ part_b/knn_modified.py
@@ -289,7 +289,66 @@ def plot_pred_premium(sparse_matrix, student_meta_data, val_data):
         "knn_modified_premium_filtering",
     )   
 
-def main():
+# Order the students by the number of questions they have answered
+# Return the user_id of these students and the number of questions they have answered
+def order_students_by_num_questions(sparse_matrix):
+    num_questions_answered = np.sum(~np.isnan(sparse_matrix), axis=1)
+    sorted_indices = np.argsort(num_questions_answered)
+    return sorted_indices, num_questions_answered[sorted_indices]
+
+# Take the bottom 20% of students who have answered the least number of questions
+# and filter the test_data to only include these students
+def filter_data_to_lowest(test_data, sorted_indices):
+    num_students = len(sorted_indices)
+    num_students_to_filter = int(num_students * 0.2)
+    students_to_filter = sorted_indices[:num_students_to_filter]
+    filtered_test_data = {
+        "user_id": [],
+        "question_id": [],
+        "is_correct": []
+    }
+    for i in range(len(test_data["user_id"])):
+        if test_data["user_id"][i] in students_to_filter:
+            filtered_test_data["user_id"].append(test_data["user_id"][i])
+            filtered_test_data["question_id"].append(test_data["question_id"][i])
+            filtered_test_data["is_correct"].append(test_data["is_correct"][i])
+    return filtered_test_data
+
+def filter_data_to_partitions(test_data, sorted_indices, num_partitions):
+    num_students = len(sorted_indices)
+    partitioned_test_data = []
+    partition_size = int(num_students / num_partitions)
+    for i in range(num_partitions):
+        partitioned_test_data.append({
+            "user_id": [],
+            "question_id": [],
+            "is_correct": []
+        })
+        partition_start = i * partition_size
+        partition_end = (i + 1) * partition_size
+        students_to_filter = sorted_indices[partition_start:partition_end]
+        for j in range(len(test_data["user_id"])):
+            if test_data["user_id"][j] in students_to_filter:
+                partitioned_test_data[i]["user_id"].append(test_data["user_id"][j])
+                partitioned_test_data[i]["question_id"].append(test_data["question_id"][j])
+                partitioned_test_data[i]["is_correct"].append(test_data["is_correct"][j])
+    return partitioned_test_data
+
+# Select only the students in the test data who have premium_pupil values that are not nan
+def filter_data_to_premium(test_data, student_meta_data):
+    filtered_test_data = {
+        "user_id": [],
+        "question_id": [],
+        "is_correct": []
+    }
+    for i in range(len(test_data["user_id"])):
+        if student_meta_data["premium_pupil"][test_data["user_id"][i]] != None:
+            filtered_test_data["user_id"].append(test_data["user_id"][i])
+            filtered_test_data["question_id"].append(test_data["question_id"][i])
+            filtered_test_data["is_correct"].append(test_data["is_correct"][i])
+    return filtered_test_data
+
+def full_modified_analysis():
     # Get the root of the project
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -299,16 +358,16 @@ def main():
     test_data = load_public_test_csv(os.path.join(project_root, "data"))
     student_meta_data = load_student_csv(os.path.join(project_root, "data"))
 
-    # year_array = get_year_array(student_meta_data)
-    # year_sparse_matrix = append_data_as_new_question(sparse_matrix, year_array)
-    # # plot_pred_year(year_array, year_sparse_matrix, val_data)
+    year_array = get_year_array(student_meta_data)
+    year_sparse_matrix = append_data_as_new_question(sparse_matrix, year_array)
+    plot_pred_year(year_array, year_sparse_matrix, val_data)
 
-    # month_array = get_month_array(student_meta_data)
-    # month_sparse_matrix = append_data_as_new_question(sparse_matrix, month_array)
-    # plot_pred_month(month_array, month_sparse_matrix, val_data)
+    month_array = get_month_array(student_meta_data)
+    month_sparse_matrix = append_data_as_new_question(sparse_matrix, month_array)
+    plot_pred_month(month_array, month_sparse_matrix, val_data)
 
-    # plot_pred_gender(sparse_matrix, student_meta_data, val_data)
-    # plot_pred_premium(sparse_matrix, student_meta_data, val_data)
+    plot_pred_gender(sparse_matrix, student_meta_data, val_data)
+    plot_pred_premium(sparse_matrix, student_meta_data, val_data)
 
     best_year_sparse_matrix = append_data_as_new_question(sparse_matrix, get_year_array(student_meta_data))
     best_year = knn_impute_by_user(best_year_sparse_matrix, test_data, 11)
@@ -327,21 +386,77 @@ def main():
     print("Best Gender Accuracy: ", best_gender)
     
     sparse_matrix = append_data_as_new_question(sparse_matrix, get_premium_array(student_meta_data) * 1.0)
-
+    sparse_matrix = append_data_as_new_question(sparse_matrix, get_gender_array(student_meta_data) * 0.5)
+    sparse_matrix = append_data_as_new_question(sparse_matrix, get_year_array(student_meta_data) * 0.1)
     combined_acc = knn_impute_by_user(sparse_matrix, test_data, 11)
     print("Combined Accuracy: ", combined_acc)
 
+def main():
+    # Get the root of the project
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-    #####################################################################
-    # TODO:                                                             #
-    # Compute the validation accuracy for each k. Then pick k* with     #
-    # the best performance and report the test accuracy with the        #
-    # chosen k*.                                                        #
-    #####################################################################
+    # Update the paths to the data files
+    sparse_matrix = load_train_sparse(os.path.join(project_root, "data")).toarray()
+    val_data = load_valid_csv(os.path.join(project_root, "data"))
+    test_data = load_public_test_csv(os.path.join(project_root, "data"))
+    student_meta_data = load_student_csv(os.path.join(project_root, "data"))
 
-    #####################################################################
-    #                       END OF YOUR CODE                            #
-    #####################################################################
+    sorted_indices, num_questions_answered = order_students_by_num_questions(sparse_matrix)
+    premium_test_data = filter_data_to_premium(test_data, student_meta_data)
+    premium_sparse = append_data_as_new_question(sparse_matrix, get_premium_array(student_meta_data) * 1)
+
+    num_partitions = 5
+    partitioned_test_data = filter_data_to_partitions(premium_test_data, sorted_indices, num_partitions)
+    table_result_values = []
+    for i in range(num_partitions):
+        print('\n\n')
+        print("Original on Partition ", i)
+        og_acc = knn_impute_by_user(sparse_matrix, partitioned_test_data[i], 11)
+        print("Premium on Partition ", i)
+        new_acc = knn_impute_by_user(premium_sparse, partitioned_test_data[i], 11)
+        table_result_values.append((i, og_acc, new_acc))
+
+    # Create plot using table_result_values
+    plt.figure(figsize=(10, 6))
+    plt.plot([str(i) for i in range(num_partitions)], [val[1] for val in table_result_values], label="Original")
+    plt.plot([str(i) for i in range(num_partitions)], [val[2] for val in table_result_values], label="Premium")
+    plt.xlabel("Partition")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy of Original and Premium on Test Data with premium_pupil status Partitioned by Number of Questions Answered")
+    plt.legend()
+    plt.savefig("../Static/knn_modified_comparison_accuracy_premium_only.png", dpi=400)
+    plt.show()
+
+
+    print('\n\n')
+    print("Original on All")
+    knn_impute_by_user(sparse_matrix, premium_test_data, 11)
+    print("Premium on All")
+    knn_impute_by_user(premium_sparse, premium_test_data, 11)
+    
+    # Repeat everything above but this time using test_data instead of premium_test_data
+    partitioned_test_data = filter_data_to_partitions(test_data, sorted_indices, num_partitions)
+    table_result_values = []
+    for i in range(num_partitions):
+        print('\n\n')
+        print("Original on Partition ", i)
+        og_acc = knn_impute_by_user(sparse_matrix, partitioned_test_data[i], 11)
+        print("Premium on Partition ", i)
+        new_acc = knn_impute_by_user(premium_sparse, partitioned_test_data[i], 11)
+        table_result_values.append((i, og_acc, new_acc))
+
+    # Create plot using table_result_values
+
+    plt.figure(figsize=(10, 6))
+    plt.plot([str(i) for i in range(num_partitions)], [val[1] for val in table_result_values], label="Original")
+    plt.plot([str(i) for i in range(num_partitions)], [val[2] for val in table_result_values], label="Premium")
+    plt.xlabel("Partition")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy of Original and Premium on all Test Data Partitioned by Number of Questions Answered")
+    plt.legend()
+    plt.savefig("../Static/knn_modified_comparison_accuracy_all.png", dpi=400)
+    plt.show()
+
 
 
 if __name__ == "__main__":
